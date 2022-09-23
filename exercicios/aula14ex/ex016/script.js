11c2fbdcbf5bce5db0ba8bba7855d6f20aff2392
Adriel Nazar Santos <99226370+adrielnazar@users.noreply.github.com>
2022-09-22 22:39:11 -0300

    termino do exercício 16

--- exercicios/aula14ex/ex016/script.js
+++ exercicios/aula14ex/ex016/script.js
@@ -3,14 +3,30 @@ function contar() {
     var fim = window.document.getElementById('ifim')
     var passo = window.document.getElementById('ipasso')
     var res = window.document.getElementById('res')
-    res.innerHTML = ''
-    Number(inicio = Number(inicio.value))
-    Number(fim = Number(fim.value))
-    Number(passo = Number(passo.value))
-    for (; inicio <= fim; inicio += passo) {
-        res.innerHTML += `Contando:<br> ${inicio}`
-
+    
+    if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        res.innerHTML = `Impossivel contar!`
+    } else {
+        res.innerHTML = 'Contando: <br>'
+        var contador = 0
+        inicio = Number(inicio.value)
+        fim = Number(fim.value)
+        passo = Number(passo.value)
+        contador = Number(inicio)
+        if (passo <= 0) {
+            passo = 1
+            window.alert('Passo invalido! Considerando PASSO 1')
+        }
+        if (inicio < fim) {
+            for (; inicio <= fim; inicio += passo) {
+                res.innerHTML += ` ${inicio} \u{1F449}`
+            }
+        } else {
+            for (; inicio >= fim; inicio -= passo) {
+                res.innerHTML += ` ${inicio} \u{1F449}`
+            }
+        }
+        res.innerHTML += `\u{1F3C1}`
     }
-    res.innerHTML += `#U+1F3C1`
 
 }
--- exercicios/aula14ex/ex017/script.js
+++ exercicios/aula14ex/ex017/script.js
@@ -1,1 +1,9 @@
-alert('Olá')
+function tabuada() {
+    var numero = window.document.getElementById('inumero')
+    var res = window.document.getElementById('ires')
+    numero = Number(numero.value)
+    res.innerHTML = ''
+    for (var multiplicador = 1; multiplicador <= 10 ; multiplicador++) {
+        res.innerHTML += (`${numero} X ${multiplicador} = ${multiplicador*numero}<br>`)
+    }
+}
